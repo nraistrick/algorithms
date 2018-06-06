3bb132ffc49582df3817219d3871040f2684e70d
Nicholas Raistrick <nraistrick123@gmail.com>
2018-06-06 20:08:47 +0100

    Added algorithms to remove duplicates from and sort singly linked-lists

---
+++ linked_list_algorithms.py
@@ -0,0 +1,161 @@
+"""
+Contains a selection of linked-list related algorithms
+"""
+
+
+class Node(object):
+    """
+    Represents a single node in a singly linked-list
+
+    :param int value: The value of the item
+    :param Node child: A reference to the next item in the list
+    """
+    def __init__(self, value, child=None):
+        self.value = value
+        self.child = child
+
+
+def remove_duplicates(node):
+    """
+    Removes all duplicate nodes from an unsorted linked-list. This is
+    achieved using a temporary buffer to store previously seen values.
+
+    * This implementation's runtime is O(n)
+    * Its space complexity is O(n)
+
+    :param Node node: The head of the linked list
+    """
+    values_seen = set()
+    while node.child:
+        if node.child.value in values_seen:
+            node.child = node.child.child
+        else:
+            values_seen.add(node.value)
+            node = node.child
+
+
+def remove_duplicates_without_buffer(node):
+    """
+    Removes duplicates from an unsorted linked-list without using a temporary
+    buffer.
+
+    * This implementation's runtime is O(n^2)
+    * Its space complexity is O(1)
+
+    :param Node node: The head of the linked list
+    """
+    head = node
+    while head:
+        while node:
+            if node.child and node.child.value == head.value:
+                node.child = node.child.child
+            else:
+                node = node.child
+        head = head.child
+        node = head
+
+
+def remove_duplicates_with_sorting(node):
+    """
+    Removes duplicates from an unsorted linked-list without using a temporary
+    buffer. This is achieved by first sorting the list. As such a side-effect
+    of this algorithm is that we lose the original ordering of the list.
+
+    * This implementation's runtime is O(n^2)
+    * Its space complexity is O(1)
+
+    :param Node node: The head of the linked list
+    """
+    sort(node)
+    while node.child:
+        if node.value == node.child.value:
+            node.child = node.child.child
+        else:
+            node = node.child
+
+
+def sort(node):
+    """
+    Sorts a singly linked-list
+
+    * This implementation's runtime is O(n^2)
+    * Its space complexity is O(1)
+
+    :param Node node: The head node of the linked list
+    """
+    head = node
+    while node.child:
+        if node.value > node.child.value:
+            # Switch the order of the two values and jump back to the start
+            node.value, node.child.value = node.child.value, node.value
+            node = head
+        else:
+            node = node.child
+
+
+def get_unique(node, previous=None):
+    """
+    Get's all unique nodes in a singly-linked list using a recursive approach.
+    For a given value, the first node in the list is kept and all subsequent
+    nodes are removed. This uses a hash-table as a temporary buffer for
+    previously seen values.
+
+    * This implementation's runtime is O(n)
+    * Its space complexity is O(n)
+
+    :param Node node: The head of the linked list
+    :param set previous: The values of any previously seen nodes
+    :return: The linked list with any duplicates removed
+    :rtype: Node
+    """
+    if previous is None:
+        previous = set()
+
+    # Only keep the last node in a list if we've not seen it before
+    if not node.child:
+        return node if node.value not in previous else None
+
+    # Get all remaining unique nodes in the list
+    if node.value not in previous:
+        previous.add(node.value)
+        node.child = get_unique(node.child, previous)
+    else:
+        node = get_unique(node.child, previous)
+
+    return node
+
+
+def get_unique_reverse(node):
+    """
+    Get's all unique nodes in a singly-linked list using a recursive approach.
+    For a given value, the last node in the list is kept and all prior
+    nodes are removed.
+
+    * This implementation's runtime is O(n^2)
+    * Its space complexity is O(n)
+
+    :param Node node: The head of the linked list
+    :return: The linked list with any duplicates removed
+    :rtype: Node
+    """
+    if not node.child:
+        return node
+
+    unique = get_unique_reverse(node.child)
+
+    # Check this node is not seen later in the list
+    this_node_unique = True
+    temp = unique
+    while temp:
+        if temp.value == node.value:
+            this_node_unique = False
+            break
+        temp = temp.child
+
+    # Only include this node in the list if it doesn't have a child
+    # with the same value
+    if this_node_unique:
+        node.child = unique
+        return node
+    else:
+        return unique
---
+++ test/test_linked_list_algorithms.py
@@ -0,0 +1,125 @@
+import unittest
+
+from linked_list_algorithms import \
+    get_unique, \
+    get_unique_reverse, \
+    Node, \
+    remove_duplicates, \
+    remove_duplicates_with_sorting, \
+    remove_duplicates_without_buffer, \
+    sort
+
+
+class TestLinkedListAlgorithms(unittest.TestCase):
+    def test_remove_duplicates(self):
+        """
+        Checks that duplicates are removed correctly from an unsorted
+        singly linked-list
+        """
+        duplicate_values = (1, 0, 1, 3, 6, 1, 1)
+        duplicates_removed = (1, 0, 3, 6)
+
+        head = self.create_linked_list(duplicate_values)
+        remove_duplicates(head)
+
+        # Confirm duplicates are removed successfully
+        self.verify_linked_list_values(head, duplicates_removed)
+
+    def test_remove_duplicates_without_buffer(self):
+        """
+        Checks that duplicates are removed correctly from an unsorted
+        singly linked-list, without the use of a temporary buffer.
+        """
+        duplicate_values = (1, 0, 1, 3, 6, 1, 1)
+        duplicates_removed = (1, 0, 3, 6)
+
+        head = self.create_linked_list(duplicate_values)
+        remove_duplicates_without_buffer(head)
+
+        self.verify_linked_list_values(head, duplicates_removed)
+
+    def test_remove_duplicates_with_sorting(self):
+        """
+        Checks that duplicates are removed correctly from an unsorted
+        singly linked-list. In this case, the list is first sorted, making
+        it possible to remove duplicates without the requirement for a
+        temporary buffer. As a trade-off, we lose the original ordering
+        of the list.
+        """
+        duplicate_values = (1, 0, 1, 3, 6, 1, 1)
+        duplicates_removed = (0, 1, 3, 6)
+
+        head = self.create_linked_list(duplicate_values)
+        remove_duplicates_with_sorting(head)
+
+        self.verify_linked_list_values(head, duplicates_removed)
+
+    def test_sort_singly_linked_list(self):
+        """
+        Checks we correctly sort a singly linked-list
+        """
+        unsorted_values = (4, 3, 5, 1, 2)
+        sorted_values = (1, 2, 3, 4, 5)
+
+        head = self.create_linked_list(unsorted_values)
+        sort(head)
+
+        self.verify_linked_list_values(head, sorted_values)
+
+    def test_get_unique(self):
+        """
+        Checks that duplicates are removed correctly from an unsorted
+        singly linked-list. For a given value, the first node in the
+        list is kept and all subsequent nodes are removed.
+        """
+        duplicate_values = (1, 0, 1, 3, 6, 1, 1)
+        duplicates_removed = (1, 0, 3, 6)
+
+        head = self.create_linked_list(duplicate_values)
+        head = get_unique(head)
+
+        self.verify_linked_list_values(head, duplicates_removed)
+
+    def test_get_unique_reverse(self):
+        """
+        Checks that duplicates are removed correctly from an unsorted
+        singly linked-list. For a given value, the last node in the
+        list is kept and all previous nodes are removed.
+        """
+        duplicate_values = (1, 0, 1, 3, 6, 1, 1)
+        duplicates_removed = (0, 3, 6, 1)
+
+        head = self.create_linked_list(duplicate_values)
+        head = get_unique_reverse(head)
+
+        self.verify_linked_list_values(head, duplicates_removed)
+
+    @staticmethod
+    def create_linked_list(values):
+        """
+        Creates a linked list from the provided iterable of values
+
+        :param collections.Iterable values: A collection of node values
+        :return: The head of the created linked list
+        :rtype: Node
+        """
+        current = head = Node(values[0])
+        for i in values[1:]:
+            current.child = Node(i)
+            current = current.child
+
+        return head
+
+    def verify_linked_list_values(self, head, values):
+        """
+        Checks the values in the provided linked-list match those in the
+        provided collection
+
+        :param Node head: The head of a linked-list
+        :param tuple values: A collection of node values
+        """
+        for i in range(len(values)):
+            self.assertEqual(values[i], head.value)
+            if not head.child:
+                break
+            head = head.child
