1c8250a6b2cc876db9556b150df49e28f7e27d88
Nicholas Raistrick <nraistrick123@gmail.com>
2018-06-10 17:56:25 +0100

    Added implementation for storing multiple stacks using a single list

---
+++ stack_queue_algorithms.py
@@ -0,0 +1,128 @@
+"""
+Contains a selection of stack and queue related algorithms
+"""
+
+
+class MultiStack(object):
+    """
+    Maintains multiple stacks and store them simultaneously on a single,
+    internal, shared list. It works under the following principles:
+
+    * The first N entries represent a pointer to the top of each supported
+    stack where we have N stacks in total
+    * Several stacks share the same underlying data structure. A single stack
+    entry is a group of three values which includes the value itself, the stack ID
+    the entry belongs to and a pointer to the last previous entry in the same
+    stack. The latter assists with updating a stack's pointer after a pop operation.
+
+    Example:
+
+    T = Pointer to top of each stack
+    I = Unique stack ID that each entry belongs to
+    P = Number of entries away from the previous entry in the stack
+    V = Value
+
+                         (stack entry)
+                         <------------> <------------> <-----------> <----------> <---------->
+    Index:   | 0   1  2 | 3  4  5      | 6  7  8      | 9  10 11    | 12 13 14   | 15 16  17  |
+    Identity | T   T  T | I  P  V      | I  P  V      | I  P  V     | I  P  V    | I  P   V   |
+    ---------+----------+--------------+--------------+-------------+------------+------------+----
+    Entries: |[17, 0, 14, 0, 0, "hello", 0, 1, "world", 0, 1, "this", 2, 0, "abc", 0, 2, "is" ...]
+    """
+    EMPTY_STACK = 0        # The value of each stack pointer if it has no entries
+    NO_PREVIOUS_ENTRY = 0  # The default value to point back to for the first stack entry
+    ENTRY_SIZE = 3         # The standard size for a single stack entry
+
+    def __init__(self, number_of_stacks):
+        """
+        :param int number_of_stacks: The number of stacks you want to maintain
+        """
+        self.number_of_stacks = number_of_stacks
+        self.entries = [self.EMPTY_STACK for _ in range(number_of_stacks)]
+
+    def push(self, stack_id, value):
+        """
+        Adds an entry to the top of the specified stack
+
+        :param int stack_id: The unique, numeric stack identifier
+        :param value: A value to push onto the stack
+        """
+        self._check_stack_id(stack_id)
+
+        top_of_stack = self.entries[stack_id]
+        if top_of_stack == self.EMPTY_STACK:
+            distance_from_last_entry = self.NO_PREVIOUS_ENTRY
+        else:
+            distance_from_last_entry = ((len(self.entries) - top_of_stack)/self.ENTRY_SIZE) + 1
+
+        self.entries.append(stack_id)
+        self.entries.append(distance_from_last_entry)
+        self.entries.append(value)
+
+        # Update the stack-specific pointer to point to the new stack head
+        self.entries[stack_id] = len(self.entries) - 1
+
+    def pop(self, stack_id):
+        """
+        Removes and retrieves an entry from the top of the specified stack
+
+        :param int stack_id: The unique, numeric stack identifier
+        :return: The top stack value
+        """
+        self._check_stack_id(stack_id)
+
+        top = self.entries[stack_id]
+        if top == self.EMPTY_STACK:
+            raise ValueError("No entries left on stack with ID %d" % stack_id)
+
+        # Get and remove stack entry
+        entry = self.entries.pop(top)
+        previous = self.entries.pop(top - 1)
+        assert self.entries.pop(top - 2) == stack_id
+
+        # Update pointer to new top of stack
+        if previous == self.NO_PREVIOUS_ENTRY:
+            self.entries[stack_id] = self.EMPTY_STACK
+        else:
+            self.entries[stack_id] -= previous * self.ENTRY_SIZE
+
+        self._update_other_stack_pointers(stack_id, top)
+
+        return entry
+
+    def _check_stack_id(self, stack_id):
+        """
+        Checks we have support for the specific stack ID
+
+        :param int stack_id: The unique, numeric stack identifier
+        """
+        if stack_id >= self.number_of_stacks:
+            raise ValueError("No support for a stack ID of %d" % stack_id)
+
+    def _update_other_stack_pointers(self, stack_id, top):
+        """
+        Update pointers for entries in other stacks which have been
+        affected by removal of an entry from the current stack.
+        For every stack except the current, the first entry it holds that
+        follows the removed entry must have its previous-entry pointer decreased
+        by one.
+
+        :param int stack_id: The ID of the stack which has had an element removed
+        :param int top: The top of the specified stack prior to having an
+        element removed
+        """
+        stack_seen = [False for _ in range(self.number_of_stacks)]
+        stack_seen[stack_id] = True
+
+        for i in range(0, len(self.entries[top - 2:]), self.ENTRY_SIZE):
+            previous_pointer = i + top - 1
+            recorded_stack_id = self.entries[previous_pointer - 1]
+
+            # We only update the first following entry of any other stack
+            if stack_seen[recorded_stack_id]:
+                continue
+
+            stack_seen[recorded_stack_id] = True
+            self.entries[recorded_stack_id] -= self.ENTRY_SIZE
+            if self.entries[previous_pointer] != self.NO_PREVIOUS_ENTRY:
+                self.entries[previous_pointer] -= 1
---
+++ test/test_stack_queue_algorithms.py
@@ -0,0 +1,156 @@
+import unittest
+
+from stack_queue_algorithms import \
+    MultiStack
+
+
+class TestStackQueueAlgorithms(unittest.TestCase):
+    def test_multi_stack_invalid_stack(self):
+        """
+        Checks we can't push entries onto non-existent stacks
+        """
+        stack = MultiStack(2)
+        self.assertRaises(ValueError, stack.push, 2, "test")
+        self.assertRaises(ValueError, stack.push, 3, "test")
+        self.assertRaises(ValueError, stack.push, 4, "test")
+
+    def test_multi_stack_empty(self):
+        """
+        Checks each empty stack responds correctly to requests
+        """
+        stack = MultiStack(3)
+
+        self.assertItemsEqual([0, 0, 0], stack.entries)
+
+        self.assertRaises(ValueError, stack.pop, 0)
+        self.assertRaises(ValueError, stack.pop, 1)
+        self.assertRaises(ValueError, stack.pop, 2)
+        self.assertRaises(ValueError, stack.pop, 3)
+        self.assertRaises(ValueError, stack.pop, 10)
+
+        self.assertRaises(ValueError, stack.push, 3, "blah")
+        self.assertRaises(ValueError, stack.push, 10, "blah")
+
+    def test_multi_stack_one_stack(self):
+        """
+        Checks the multi-stack model performs correctly with one stack
+        """
+        stack = MultiStack(1)
+
+        stack.push(0, "this")
+        stack.push(0, "is")
+        stack.push(0, "pizza")
+        stack.push(0, "with")
+        stack.push(0, "cheese")
+
+        self.assertEqual("cheese", stack.pop(0))
+        self.assertEqual("with", stack.pop(0))
+        self.assertEqual("pizza", stack.pop(0))
+        self.assertEqual("is", stack.pop(0))
+        self.assertEqual("this", stack.pop(0))
+        self.assertRaises(ValueError, stack.pop, 0)
+
+        self.assertItemsEqual([0], stack.entries)
+
+    def test_multi_stack_two_stacks(self):
+        """
+        Checks the multi-stack model performs correctly with two stacks
+        """
+        stack = MultiStack(2)
+
+        stack.push(1, "abc")
+        stack.push(0, "this")
+        stack.push(0, "is")
+        stack.push(1, "def")
+        stack.push(0, "pizza")
+        stack.push(0, "with")
+        stack.push(1, "ghi")
+        stack.push(0, "cheese")
+
+        self.assertEqual("ghi", stack.pop(1))
+        self.assertEqual("def", stack.pop(1))
+        self.assertEqual("abc", stack.pop(1))
+        self.assertRaises(ValueError, stack.pop, 1)
+
+        self.assertEqual("cheese", stack.pop(0))
+        self.assertEqual("with", stack.pop(0))
+        self.assertEqual("pizza", stack.pop(0))
+        self.assertEqual("is", stack.pop(0))
+        self.assertEqual("this", stack.pop(0))
+        self.assertRaises(ValueError, stack.pop, 0)
+
+        self.assertItemsEqual([0, 0], stack.entries)
+
+    def test_multi_stack_three_stacks(self):
+        """
+        Checks the multi-stack model performs correctly with three stacks
+        """
+        stack = MultiStack(3)
+
+        stack.push(1, "abc")
+        stack.push(0, "this")
+        stack.push(0, "is")
+        stack.push(1, "def")
+        stack.push(2, 135678)
+        stack.push(2, 99)
+        stack.push(0, "pizza")
+        stack.push(0, "with")
+        stack.push(0, "cheese")
+
+        self.assertEqual("def", stack.pop(1))
+        self.assertEqual("abc", stack.pop(1))
+        self.assertRaises(ValueError, stack.pop, 1)
+
+        self.assertEqual("cheese", stack.pop(0))
+        self.assertEqual("with", stack.pop(0))
+        self.assertEqual("pizza", stack.pop(0))
+        self.assertEqual("is", stack.pop(0))
+        self.assertEqual("this", stack.pop(0))
+        self.assertRaises(ValueError, stack.pop, 0)
+
+        self.assertEqual(99, stack.pop(2))
+        self.assertEqual(135678, stack.pop(2))
+        self.assertRaises(ValueError, stack.pop, 2)
+
+        self.assertItemsEqual([0, 0, 0], stack.entries)
+
+    def test_multi_stack_four_stacks(self):
+        """
+        Checks the multi-stack model performs correctly with four stacks
+        """
+        stack = MultiStack(4)
+
+        stack.push(3, (4, 5, 6))
+        stack.push(0, "this")
+        stack.push(2, "abc")
+        stack.push(2, "def")
+        stack.push(0, "is")
+        stack.push(1, 135678)
+        stack.push(1, 99)
+        stack.push(0, "pizza")
+        stack.push(0, "with")
+        stack.push(0, "extra")
+        stack.push(0, "cheese")
+        stack.push(3, (7, 8, 9))
+
+        self.assertEqual("def", stack.pop(2))
+        self.assertEqual("abc", stack.pop(2))
+        self.assertRaises(ValueError, stack.pop, 2)
+
+        self.assertEqual("cheese", stack.pop(0))
+        self.assertEqual("extra", stack.pop(0))
+        self.assertEqual("with", stack.pop(0))
+        self.assertEqual("pizza", stack.pop(0))
+        self.assertEqual("is", stack.pop(0))
+        self.assertEqual("this", stack.pop(0))
+        self.assertRaises(ValueError, stack.pop, 0)
+
+        self.assertEqual(99, stack.pop(1))
+        self.assertEqual(135678, stack.pop(1))
+        self.assertRaises(ValueError, stack.pop, 1)
+
+        self.assertEqual((7, 8, 9), stack.pop(3))
+        self.assertEqual((4, 5, 6), stack.pop(3))
+        self.assertRaises(ValueError, stack.pop, 3)
+
+        self.assertItemsEqual([0, 0, 0, 0], stack.entries)
