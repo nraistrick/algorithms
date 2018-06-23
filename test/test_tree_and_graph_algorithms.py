b9f73c7cf262054ea237bfcab2feb3eb7444ddff
Nicholas Raistrick <nraistrick123@gmail.com>
2018-06-23 20:08:35 +0100

    Added algorithms to depth and breadth-first-search a directed graph

--- test/test_tree_and_graph_algorithms.py
+++ test/test_tree_and_graph_algorithms.py
@@ -2,6 +2,9 @@ import unittest
 
 from tree_and_graph_algorithms import \
     BinaryNode, \
+    breadth_first_search, \
+    depth_first_search, \
+    GraphVertex, \
     traverse_binary_tree, \
     traverse_binary_tree_post_order, \
     traverse_binary_tree_pre_order
@@ -30,4 +33,88 @@ class TestTreeAndGraphAlgorithms(unittest.TestCase):
 
         post_ordered = [v for v in traverse_binary_tree_post_order(tree)]
         self.assertEqual([1, 4, 3, 6, 10, 9, 5], post_ordered)
+
+    def test_graph_depth_first_search(self):
+        """
+        Checks we correctly find values in a graph using a depth-first
+        search approach
+        """
+        # Set up a binary tree
+        first = GraphVertex(1)
+        second = GraphVertex(2)
+        third = GraphVertex(3)
+        fourth = GraphVertex(4)
+
+        first.vertices.extend([second, fourth])
+        second.vertices.extend([third, fourth])
+        third.vertices.append(first)
+        fourth.vertices.append(second)
+
+        # Perform searches
+        found, vertex, nodes_visited = depth_first_search(first, 1)
+        self.assertTrue(found)
+        self.assertEqual(1, vertex.value)
+        self.assertEqual(1, nodes_visited)
+
+        found, vertex, nodes_visited = depth_first_search(first, 2)
+        self.assertTrue(found)
+        self.assertEqual(2, vertex.value)
+        self.assertEqual(2, nodes_visited)
+
+        found, vertex, nodes_visited = depth_first_search(first, 3)
+        self.assertTrue(found)
+        self.assertEqual(3, vertex.value)
+        self.assertEqual(3, nodes_visited)
+
+        found, vertex, nodes_visited = depth_first_search(first, 4)
+        self.assertTrue(found)
+        self.assertEqual(4, vertex.value)
+        self.assertEqual(3, nodes_visited)
+
+        found, vertex, nodes_visited = depth_first_search(first, 10)
+        self.assertFalse(found)
+        self.assertIsNone(vertex)
+        self.assertIsNone(nodes_visited)
+
+    def test_graph_depth_first_search(self):
+        """
+        Checks we correctly find values in a graph using a breadth-first
+        search approach
+        """
+        # Set up a directed graph
+        first = GraphVertex(1)
+        second = GraphVertex(2)
+        third = GraphVertex(3)
+        fourth = GraphVertex(4)
+
+        first.vertices.extend([second, fourth])
+        second.vertices.extend([third, fourth])
+        third.vertices.append(first)
+        fourth.vertices.append(second)
+
+        # Perform searches
+        found, vertex, nodes_visited = breadth_first_search(first, 1)
+        self.assertTrue(found)
+        self.assertEqual(1, vertex.value)
+        self.assertEqual(1, nodes_visited)
+
+        found, vertex, nodes_visited = breadth_first_search(first, 2)
+        self.assertTrue(found)
+        self.assertEqual(2, vertex.value)
+        self.assertEqual(2, nodes_visited)
+
+        found, vertex, nodes_visited = breadth_first_search(first, 3)
+        self.assertTrue(found)
+        self.assertEqual(3, vertex.value)
+        self.assertEqual(4, nodes_visited)
+
+        found, vertex, nodes_visited = breadth_first_search(first, 4)
+        self.assertTrue(found)
+        self.assertEqual(4, vertex.value)
+        self.assertEqual(3, nodes_visited)
+
+        found, vertex, nodes_visited = breadth_first_search(first, 10)
+        self.assertFalse(found)
+        self.assertIsNone(vertex)
+        self.assertIsNone(nodes_visited)
 
--- tree_and_graph_algorithms.py
+++ tree_and_graph_algorithms.py
@@ -2,6 +2,8 @@
 Contains a selection of tree and graph related algorithms
 """
 
+from Queue import Queue
+
 
 class BinaryNode(object):
     """
@@ -79,3 +81,76 @@ def traverse_binary_tree_post_order(tree):
 
     yield tree.value
 
+
+class GraphVertex(object):
+    def __init__(self, value, vertices=None):
+        """
+        Represents a single vertex in a graph
+
+        :param int value: The value of the vertex
+        :param list[GraphVertex] vertices: A collection of other vertices that
+        are accessible from this vertex by an edge
+        """
+        self.value = value
+        self.vertices = vertices if vertices else []
+
+
+def depth_first_search(vertex, value, visited=None):
+    """
+    Perform a depth first search through a directed graph
+
+    :param GraphVertex vertex: The start vertex in the graph
+    :param int value: The value to search for
+    :param list[int] visited: A collection of previous values visited
+    :return: A boolean describing whether the vertex was found, the vertex itself,
+    and the number of nodes visited to find the specified node
+    :rtype: tuple[boolean, GraphVertex, int]
+    """
+    if vertex.value == value:
+        nodes_visited = len(visited) + 1 if visited else 1
+        return True, vertex, nodes_visited
+
+    if visited is None:
+        visited = []
+
+    for v in vertex.vertices:
+        if v.value in visited:
+            continue
+
+        updated_visited = [vertex.value]
+        updated_visited.extend(visited)
+
+        found, found_vertex, nodes_visited = depth_first_search(v, value, updated_visited)
+        if found:
+            return found, found_vertex, nodes_visited
+
+    return False, None, len(visited)
+
+
+def breadth_first_search(vertex, value):
+    """
+    Perform a breadth first search through a directed graph
+
+    :param GraphVertex vertex: The start vertex in the graph
+    :param int value: The value to search for
+    :return: A boolean describing whether the vertex was found, the vertex itself,
+    and the number of nodes traversed to find the specified node
+    :rtype: tuple[boolean, GraphVertex, int]
+    """
+    queue = Queue()
+    queue.put((vertex, []))
+
+    while not queue.empty():
+
+        vertex, visited = queue.get()
+        if vertex.value in visited:
+            continue
+        elif vertex.value == value:
+            return True, vertex, len(visited) + 1
+
+        visited.append(vertex.value)
+
+        for v in vertex.vertices:
+            queue.put((v, visited))
+
+    return False, None, None
