75977b7e2a2c5e80edd9bc1e4ff16f5b901c213a
Nicholas Raistrick <nraistrick123@gmail.com>
2018-07-03 07:48:14 +0100

    Added algorithm to print numbers between 0 and 1 in binary

--- bit_manipulation_algorithms.py
+++ bit_manipulation_algorithms.py
@@ -2,6 +2,8 @@
 Contains a selection of bit manipulation related algorithms
 """
 
+from cStringIO import StringIO
+
 
 def insert_bits(first, second, i, j):
     """
@@ -31,3 +33,59 @@ def insert_bits(first, second, i, j):
     first |= second
 
     return first
+
+
+def binary_to_string(value):
+    """
+    Prints a floating point number between 0 and 1 in its binary form
+
+    If the number cannot be represented accurately in 32 characters or less,
+    an error will be printed instead
+
+    :param float value: The value to print as binary. Must be between 0 and 1.
+    """
+    # The maximum number of floating point characters to display as binary
+    max_characters = 32
+
+    # Can ignore numbers smaller than this
+    required_accuracy = 0.00001
+
+    if value < 0 or value > 1:
+        raise ValueError("Invalid input argument")
+
+    characters = ['0', '.']
+    while len(characters) < max_characters:
+        value *= 10
+
+        v = int(value)
+        characters.append(str(v))
+        value -= v
+
+        if value < required_accuracy:
+            break
+
+    if value > required_accuracy:
+        raise ValueError("Number cannot be represented accurately within %d digits" %
+                         max_characters)
+
+    bits = StringIO()
+    for c in characters:
+        for bit_number in range(7, -1, -1):
+            bits.write(str(get_bit(ord(c), bit_number)))
+
+    return bits.getvalue()
+
+
+def get_bit(value, bit):
+    """
+    Get's a specific bit from the provided number
+
+    :param int value: A number to get the bit of
+    :param bit: The number of the bit
+    :return: The bit value, 0 or 1
+    :rtype: int
+    """
+    if bit < 0:
+        raise ValueError("Bit number must be 0 or greater")
+
+    return 1 if value & 1 << bit != 0 else 0
--- test/test_bit_manipulation.py
+++ test/test_bit_manipulation.py
@@ -1,6 +1,8 @@
 import unittest
 
 from bit_manipulation_algorithms import \
+    binary_to_string, \
+    get_bit, \
     insert_bits
 
 
@@ -19,3 +21,41 @@ class TestBitManipulationAlgorithms(unittest.TestCase):
         actual = insert_bits(n, m, i, j)
 
         self.assertEqual(bin(expected), bin(actual))
+
+    def test_binary_to_string(self):
+        """
+        Checks we correctly convert a floating point number
+        to its binary representation
+        """
+        self.assertRaises(ValueError, binary_to_string, 2.5)
+        self.assertRaises(ValueError, binary_to_string, -1.3)
+
+        self.assertRaises(ValueError, binary_to_string, 0.0123456789012345678901234567890123456789)
+
+        self.assertEqual("001100000010111000110000",
+                         binary_to_string(0.0))
+
+        self.assertEqual("001100000010111000110101",
+                         binary_to_string(0.5))
+
+        self.assertEqual("0011000000101110001100010011001000110101",
+                         binary_to_string(0.125))
+
+        self.assertEqual("00110000001011100011000000110110001100100011001000110101",
+                         binary_to_string(0.06225))
+
+    def test_get_bit(self):
+        """
+        Tests getting a single bit from a provided value
+        """
+        self.assertEqual(0, get_bit(0b1000, 0))
+        self.assertEqual(0, get_bit(0b1000, 1))
+        self.assertEqual(0, get_bit(0b1000, 2))
+        self.assertEqual(1, get_bit(0b1000, 3))
+
+        self.assertEqual(0, get_bit(0b1000, 4))
+        self.assertEqual(0, get_bit(0b1000, 5))
+        self.assertEqual(0, get_bit(0b1000, 6))
+        self.assertEqual(0, get_bit(0b1000, 7))
+
+        self.assertRaises(ValueError, get_bit, 0b1000, -1)
